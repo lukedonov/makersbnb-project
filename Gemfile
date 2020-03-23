cec43f6c1bef1354a6d971cba18ba6cfe04615df
Orion Reed <orionreedt@gmail.com>
2020-03-23 17:53:39 +0000

    tweaked gemfile, added gitignore, simplecov, rubocop

--- Gemfile
+++ Gemfile
@@ -5,9 +5,13 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra"
-gem "sinatra/flash"
+gem "sinatra-flash"
 gem "solargraph"
 gem "pg"
 
 gem "capybara", group: :tests
 gem "rspec", group: :tests
+gem "rubocop", group: :tests
+gem "simplecov", group: :tests
+gem "simplecov-console", group: :tests
+gem "rdoc", group: :tests
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,19 @@ ENV['ENVIRONMENT'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
+require 'simplecov'
+require 'simplecov-console'
 require 'capybara/rspec'
 require 'rspec'
 
 Capybara.app = InnCognito
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+                                                                 SimpleCov::Formatter::Console
+                                                               ])
+SimpleCov.start do
+  add_filter "database_connection_setup.rb"
+end
 
 RSpec.configure do |config|
   config.before(:each) do
