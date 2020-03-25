c39716ec8c8df4fc05c914da07be95bae903c98a
Gareth Harris <gdharris1000@gmail.com>
2020-03-25 10:54:42 +0000

    booking and availability tables setup

---
+++ db/migrations/05_create_bookings_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bookings(id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users (id), property_id INTEGER REFERENCES properties (id), start_date TIMESTAMP, end_date TIMESTAMP, approval BOOLEAN);
---
+++ db/migrations/06_create_availability_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE availablility(id SERIAL PRIMARY KEY, property_id INTEGER REFERENCES properties (id), start_date TIMESTAMP, end_date TIMESTAMP);
