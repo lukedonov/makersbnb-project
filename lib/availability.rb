090da5ab0a59b4f552e45ed941452d81022cbaab
Sophia Bell <Kittaru87@gmail.com>
2020-03-26 12:20:17 +0000

    adding date format method and availability dates to booking page

--- lib/availability.rb
+++ lib/availability.rb
@@ -13,8 +13,18 @@ class Availability
     map(DatabaseConnection.query("INSERT INTO availability (property_id, start_date, end_date) VALUES ('#{property_id}', '#{start_date}', '#{end_date}') RETURNING id, property_id, start_date, end_date;")).first
   end
 
-  def self.map(sql_result)
-    sql_result.map { |b| Availability.new(b['id'], b['property_id'], b['start_date'], b['end_date']) }
+  def self.find(property_id:)
+    result = DatabaseConnection.query("SELECT * FROM availability WHERE property_id = #{property_id}")
+    map(result).first
   end
+
+  def self.map(sql_result)
+    sql_result.map { |b| Availability.new(b['id'], b['property_id'], date_format(b['start_date']), date_format(b['end_date']))   }
+  end
+
+  def self.date_format(date)
+    DateTime.strptime(date, '%Y-%m-%d %H:%M:%S').strftime("%d %b %Y")
+  end
+
 
 end
